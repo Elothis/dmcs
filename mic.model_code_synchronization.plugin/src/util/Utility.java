ecf9f70466c49da511bd0defa8f9b50c4974b3c0
Elothis <sihtole7@web.de>
2019-06-15 12:10:54 +0200

    Add utility method to return Map content in String format to print out

--- mic.model_code_synchronization.plugin/src/util/Utility.java
+++ mic.model_code_synchronization.plugin/src/util/Utility.java
@@ -4,7 +4,9 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 
@@ -16,6 +18,21 @@ public class Utility {
 		List<File> files = (List<File>) FileUtils.listFiles(directory, extensions, true);
 		return files;
     }
+	
+	public static String mapToString(Map mp) {
+		StringBuilder sb = new StringBuilder();
+	    Iterator it = mp.entrySet().iterator();
+	    while (it.hasNext()) {
+	        Map.Entry pair = (Map.Entry)it.next();
+	        sb.append("(")
+	        	.append(pair.getKey())
+	        	.append("->")
+	        	.append(pair.getValue())
+	        	.append(")");
+	        it.remove();
+	    }
+	    return sb.toString();
+	}
 	
 	public static void writeToFile(String fileName, String output) {
 	    try {
