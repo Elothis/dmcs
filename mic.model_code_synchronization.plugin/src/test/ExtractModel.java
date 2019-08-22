2e9592628a6591d630f8a62b1cf3f135aad072ff
Elothis <sihtole7@web.de>
2019-08-22 11:20:40 +0200

    Add extract model test case for evaluation

--- mic.model_code_synchronization.plugin/src/test/ExtractModel.java
+++ mic.model_code_synchronization.plugin/src/test/ExtractModel.java
@@ -11,16 +11,16 @@ class ExtractModel {
 
 	public static final String MAPPING_DIRECTORY_PATH = "C:/Users/Fabian/mappingDirectory";
 	public static final String PROJECT_PATH = "C:/Daten/UNI/jack3/";
+	//public static final String PROJECT_PATH = "C:/Daten/MIC_Sync_Tool_Repo/TestProject";
+	//public static final String PROJECT_PATH = "F:/code_generation";
 
 	@Test
 	void extractModel() throws IOException {
-		
+		long pre = System.currentTimeMillis();
 		MappingDeclarationParserImpl parser = new MappingDeclarationParserImpl();
 		
-		//this takes really long
 		TransformationManager tm = new TransformationManager(MAPPING_DIRECTORY_PATH, PROJECT_PATH, parser);
-		long before = System.currentTimeMillis();
 		tm.buildDesignModel();
-		System.out.println(System.currentTimeMillis() - before);
+		System.out.println("overall time : " + (System.currentTimeMillis() - pre));
 	}
 }
